947af2c8ff75cad324e518e5bcaee88b7733c641
Niels van der Zanden <niels@phusion.nl>
2019-01-21 11:50:45 +0100

    Make sure .gem dir exists

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,7 @@
 set -e
 
 if [ -n "$RUBYGEMS_AUTH_TOKEN" ]; then
+  mkdir -p $HOME/.gem/
   GEM_CREDENTIALS="$HOME/.gem/credentials"
 
   echo -e "---\n:rubygems_api_key: $RUBYGEMS_AUTH_TOKEN" > "$GEM_CREDENTIALS"
